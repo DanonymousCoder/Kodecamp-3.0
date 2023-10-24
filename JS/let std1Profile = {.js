c0fdb66d85710cd97c893e3bc005132b326e5323
Ridwan <aladebaruwa@gmail.com>
2023-10-24 11:50:01 +0100

    add

--- JS/let std1Profile = {.js
+++ JS/let std1Profile = {.js
@@ -1,10 +1,26 @@
-let std1Profile = {
+let std1Profile = [
+    {
+        first_name: "John",
+        last_name: "Dozie",
+        age: 45,
+        performance: "Excellent",
+        track: "Frontend"
+      },
+      {
+        first_name: "John",
+        last_name: "Dozie",
+        age: 45,
+        performance: "Excellent",
+        track: "Frontend"
+      }    ,
+{
   first_name: "John",
   last_name: "Dozie",
   age: 45,
   performance: "Excellent",
   track: "Frontend"
 }
+]
 let objKeys = Object.values(std1Profile);
 console.log(objKeys);
 
